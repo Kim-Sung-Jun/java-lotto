d04c3c5b3de673134855c325010ed4c87a2126d3
kim-sung-jun <dd8873@naver.com>
2021-11-03 19:01:28 +0900

    객체지향 개념이 많이 부족하다..

--- src/LottoWinningNumber.java
+++ src/LottoWinningNumber.java
@@ -3,24 +3,37 @@ import java.util.List;
 
 public class LottoWinningNumber {
 
-    private final List<Integer> lottoWinningNumbers = new ArrayList<>();
+    private static final int LENGTH_DELIMITER = 6;
+
+    private final List<Integer> lottoWinningNumbers;
 
     public LottoWinningNumber(String lottoWinningNumber) {
-        createLottoWinningNumbers(lottoWinningNumber);
+        this.lottoWinningNumbers = createLottoWinningNumbers(lottoWinningNumber);
     }
 
-    private void createLottoWinningNumbers(String lottoWinningNumber) {
+    private List<Integer> createLottoWinningNumbers(String lottoWinningNumber) {
+        List<Integer> winningNumbers = new ArrayList<>();
         String[] lottoWinningNumbers = lottoWinningNumber.trim().split(",");
+        validateLength(lottoWinningNumbers);
         for (String number : lottoWinningNumbers) {
-            validateLottoWinningNumberType(number);
+            validateLottoWinningNumberType(number, winningNumbers);
+        }
+        return winningNumbers;
+    }
+
+    private void validateLength(String[] lottoWinningNumbers) {
+        if (lottoWinningNumbers.length < LENGTH_DELIMITER) {
+            throw new IllegalArgumentException("숫자가 모자릅니다.");
+        } else if (lottoWinningNumbers.length > LENGTH_DELIMITER) {
+            throw new IllegalArgumentException("숫자가 너무 많습니다.");
         }
     }
 
-    private void validateLottoWinningNumberType(String number) {
+    private void validateLottoWinningNumberType(String number, List<Integer> winningNumbers) {
         try {
-            this.lottoWinningNumbers.add(Integer.parseInt(number));
+            winningNumbers.add(Integer.parseInt(number));
         } catch (NumberFormatException ignored) {
-            Output.printExceptionMsg("숫자가 아닙니다.");
+            throw new NumberFormatException("숫자가 아닙니다.");
         }
     }
 
